a3d97769ec6a48dfc95575d391e345cd1fb8f742
KenKwok <kenkinming2002@gmail.com>
2023-09-24 02:53:36 +0800

    Basic Expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub enum Expr {
+    Unit(String),
+    Not(Box<Expr>),
+    And(Vec<Expr>),
+    Or(Vec<Expr>),
+}
+
+impl Expr {
+    pub fn unit<S: Into<String>>(s : S) -> Self { Self::Unit(s.into()) }
+    pub fn not(expr : Self) -> Self { Self::Not(Box::new(expr)) }
+    pub fn and<I: IntoIterator<Item = Expr>>(exprs : I) -> Self { Self::And(Vec::from_iter(exprs)) }
+    pub fn or <I: IntoIterator<Item = Expr>>(exprs : I) -> Self { Self::Or( Vec::from_iter(exprs)) }
 }
 
 #[cfg(test)]
@@ -7,8 +17,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_expr() {
+        let a = Expr::unit("a");
+        let b = Expr::unit("b");
+        let c = Expr::and([a, b]);
+        let d = Expr::unit("d");
+        let e = Expr::not(d);
+        let _ = Expr::or([c, e]);
     }
 }
