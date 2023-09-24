61a676eef3d784cd131e16160b7a2530c2b25908
KenKwok <kenkinming2002@gmail.com>
2023-09-24 23:55:27 +0800

    Conjuctive and Disjunctive Normal Form

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(box_patterns)]
 #![feature(extract_if)]
 
+use itertools::Itertools;
+
 enum Either<L, R> {
     Left(L),
     Right(R),
@@ -27,6 +29,26 @@ pub enum Expr {
     Or(Vec<Expr>),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Literal {
+    ident  : String,
+    negate : bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Clause {
+    literals : Vec<Literal>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)] pub struct ConjunctiveNormalForm { clauses : Vec<Clause> }
+#[derive(Debug, Clone, PartialEq, Eq)] pub struct DisjunctiveNormalForm { clauses : Vec<Clause> }
+
+impl Literal {
+    fn negate(self) -> Self {
+        Self { ident : self.ident, negate : !self.negate }
+    }
+}
+
 impl Expr {
     pub fn unit<S: Into<String>>(s : S) -> Self { Self::Unit(s.into()) }
     pub fn not(expr : Self) -> Self { Self::Not(Box::new(expr)) }
@@ -41,6 +63,56 @@ impl Expr {
             Expr::And(exprs) => Expr::and(exprs.into_iter().flat_map(|expr| match expr { Expr::And(exprs) => Either::Left(exprs.into_iter().map(Expr::simplify)), expr => Either::Right(std::iter::once(expr.simplify())), })),
             Expr::Or (exprs) => Expr::or (exprs.into_iter().flat_map(|expr| match expr { Expr::Or (exprs) => Either::Left(exprs.into_iter().map(Expr::simplify)), expr => Either::Right(std::iter::once(expr.simplify())), })),
             expr => expr,
+        }
+    }
+
+    pub fn to_cnf(&self) -> ConjunctiveNormalForm {
+        match self {
+            Self::Unit(ident) => ConjunctiveNormalForm { clauses : vec![Clause { literals : vec![Literal { ident : ident.clone(), negate : false }] }] },
+            Self::Not(expr) => {
+                let cnf = expr.to_cnf();
+                let literals = cnf.clauses.into_iter().map(|clause| clause.literals.into_iter()); // Iterator of Iterator of Literal
+                let literals = literals.map(|literals| literals.map(Literal::negate));            // Iterator of Iterator of Literal
+                let literals = literals.multi_cartesian_product();
+                ConjunctiveNormalForm { clauses : literals.map(|literals| Clause { literals }).collect_vec() }
+            },
+            Self::And(exprs) => {
+                let cnfs = exprs.into_iter().map(|expr| expr.to_cnf());
+                let clauses = cnfs.flat_map(|cnf| cnf.clauses).collect_vec();
+                ConjunctiveNormalForm { clauses }
+            },
+            Self::Or(exprs) => {
+                let cnfs = exprs.into_iter().map(|expr| expr.to_cnf());
+                let clauses = cnfs.map(|cnf| cnf.clauses);
+                let clauses = clauses.multi_cartesian_product(); // Iterator of Iterator of Clause
+                let clauses = clauses.map(|clauses| Clause { literals : clauses.into_iter().flat_map(|clause| clause.literals).collect_vec() }).collect_vec();
+                ConjunctiveNormalForm { clauses }
+            },
+        }
+    }
+
+    pub fn to_dnf(&self) -> DisjunctiveNormalForm {
+        match self {
+            Self::Unit(ident) => DisjunctiveNormalForm { clauses : vec![Clause { literals : vec![Literal { ident : ident.clone(), negate : false }] }] },
+            Self::Not(expr) => {
+                let dnf = expr.to_dnf();
+                let literals = dnf.clauses.into_iter().map(|clause| clause.literals.into_iter()); // Iterator of Iterator of Literal
+                let literals = literals.map(|literals| literals.map(Literal::negate));            // Iterator of Iterator of Literal
+                let literals = literals.multi_cartesian_product();
+                DisjunctiveNormalForm { clauses : literals.map(|literals| Clause { literals }).collect_vec() }
+            },
+            Self::And(exprs) => {
+                let dnfs = exprs.into_iter().map(|expr| expr.to_dnf());
+                let clauses = dnfs.map(|dnf| dnf.clauses);
+                let clauses = clauses.multi_cartesian_product(); // Iterator of Iterator of Clause
+                let clauses = clauses.map(|clauses| Clause { literals : clauses.into_iter().flat_map(|clause| clause.literals).collect_vec() }).collect_vec();
+                DisjunctiveNormalForm { clauses }
+            },
+            Self::Or(exprs) => {
+                let dnfs = exprs.into_iter().map(|expr| expr.to_dnf());
+                let clauses = dnfs.flat_map(|dnf| dnf.clauses).collect_vec();
+                DisjunctiveNormalForm { clauses }
+            },
         }
     }
 }
@@ -64,7 +136,7 @@ mod tests {
                 ]),
             ]),
             Expr::not(Expr::not(Expr::not(d.clone()))),
-        ]);
+        ]).simplify();
 
         let a = Expr::unit("a");
         let b = Expr::unit("b");
@@ -76,5 +148,14 @@ mod tests {
         );
 
         assert_eq!(lhs.simplify(), rhs);
+        assert_eq!(rhs.to_cnf(), ConjunctiveNormalForm { clauses : vec![
+            Clause { literals : vec![Literal { ident : "a".into(), negate : false }, Literal { ident : "d".into(), negate : true }] },
+            Clause { literals : vec![Literal { ident : "b".into(), negate : false }, Literal { ident : "d".into(), negate : true }] },
+            Clause { literals : vec![Literal { ident : "c".into(), negate : false }, Literal { ident : "d".into(), negate : true }] },
+        ]});
+        assert_eq!(rhs.to_dnf(), DisjunctiveNormalForm { clauses : vec![
+            Clause { literals : vec![Literal { ident : "a".into(), negate : false }, Literal { ident : "b".into(), negate : false }, Literal { ident : "c".into(), negate : false }] },
+            Clause { literals : vec![Literal { ident : "d".into(), negate : true }] },
+        ]});
     }
 }
